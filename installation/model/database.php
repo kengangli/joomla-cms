0065ca01bdecc7eef180672011c5facbcbfc59bf
Ken <ken.gang.li@gmail.com>
2020-09-22 21:41:57 +1000

    _installation folder renamed back to installation and database.php file modified

--- installation/model/database.php
+++ installation/model/database.php
@@ -165,9 +165,9 @@ class InstallationModelDatabase extends JModelBase
 		if ($shouldCheckLocalhost && preg_match($localhost, $options->db_host) !== 1)
 		{
 			$remoteDbFileTestsPassed = JFactory::getSession()->get('remoteDbFileTestsPassed', false);
-			//JFactory::getSession()->set('remoteDbFileTestsPassed', true);
+			JFactory::getSession()->set('remoteDbFileTestsPassed', true);
 			// When all checks have been passed we don't need to do this here again.
-			if ($remoteDbFileTestsPassed === false)
+			/*if ($remoteDbFileTestsPassed === false)
 			{
 				$generalRemoteDatabaseMessage = JText::sprintf(
 					'INSTL_DATABASE_HOST_IS_NOT_LOCALHOST_GENERAL_MESSAGE',
@@ -258,7 +258,7 @@ class InstallationModelDatabase extends JModelBase
 
 				// All tests for this session passed set it to the session
 				JFactory::getSession()->set('remoteDbFileTestsPassed', true);
-			} 
+			} */
 		}
 
 		// Get a database object.
